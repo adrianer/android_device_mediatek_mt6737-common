279b43923c818262b15e0a2ce5bccd6d0f90f411
SamarV-121 <samarvispute121@gmail.com>
2018-08-07 02:11:25 -0400

    mt6737-common: Update properties
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- mt6737.mk
+++ mt6737.mk
@@ -106,9 +106,14 @@ PRODUCT_COPY_FILES += \
 #### Props ####
 
 # Android Debugging
-ADDITIONAL_DEFAULT_PROPERTIES += \
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.secure=0 \
-    ro.adb.secure=0
+    ro.adb.secure=0 \
+    ro.debuggable=1 \
+    persist.service.adb.enable=1
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.allow.mock.location=0
 
 #### BoardConfigs ####
 
